aef0c358d884b3973bdf69f1e8419984855d51d1
Roman Nozdrin <roman.nozdrin@mariadb.com>
2018-05-08 00:17:51 +0300

    WIP Add: modal to the game, socket.io client lib.

--- tests/test_client.js
+++ tests/test_client.js
@@ -1,26 +1,23 @@
 var io = require('socket.io-client');
 var socket = io.connect('http://localhost:8080');
-/*socket.on('printOut', function (data) {
+socket.on('printOut', function (data) {
     console.log(data);
-});*/
-socket.emit('start');
+});
+//socket.emit('start');
 socket.on('ping', function (data) {
     console.log(data);
 });
 
 //socket.emit('turn_up', { my: 'data' });
-socket.emit('turn', {playerid: 1, squareid: 25});
+//socket.emit('turn', {playerid: 1, squareid: 25});
 //socket.emit('turn', {playerid: 2, squareid: 25});
-socket.emit('turn', {playerid: 2, squareid: 81});
+/*socket.emit('turn', {playerid: 2, squareid: 81});
 socket.emit('print');
 socket.emit('turn', {playerid: 3, squareid: 23});
 socket.emit('turn', {playerid: 4, squareid: 25});
 socket.emit('turn', {playerid: 5, squareid: 56});
 socket.emit('turn', {playerid: 6, squareid: 31});
+*/
 socket.emit('print');
-
-socket.on('printOut', function (data) {
-    console.log(data);
-});
 
 setTimeout( () => { process.exit(0); }, 500);
